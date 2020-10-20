57b20eceed96610762fb1151a996d90d0ee0162e
carlosdesousa91 <31635606+carlosdesousa91@users.noreply.github.com>
2020-10-20 13:40:30 -0300

    criada fũnção verificaTicket

--- providers/Topdesk/nocPro/topdesk_class.php
+++ providers/Topdesk/nocPro/topdesk_class.php
@@ -1,20 +1,21 @@
 <?php
 
 function verificaTicket($id_relacinamento, $horadafalha){
-    $base_url = 'https://rnp.topdesk.net/tas/api/incidents?fields=id,number,optionalFields1.date1';
-    //&object_name=123&processing_status=2817418e-5afc-4a8e-b2e4-7e4ff104e095&processing_status=a3e2ad64-16e2-4fe3-9c66-9e50ad9c4d69';
+    $base_url = 'https://rnp.topdesk.net/tas/api/incidents?fields=id,number,optionalFields1.date1&object_name=123&processing_status=2817418e-5afc-4a8e-b2e4-7e4ff104e095&processing_status=a3e2ad64-16e2-4fe3-9c66-9e50ad9c4d69';
 	$ch = curl_init($base_url);
 	if ($ch == false) {
 		$this->setWsError("cannot init curl object");
 		return 1;
 	}
-	
+    
+    //este array não é usado os campos são passados todos na url
 	$argument = array(
 			
-            'processing_status' => '2817418e-5afc-4a8e-b2e4-7e4ff104e095',
-            'processing_status' => 'a3e2ad64-16e2-4fe3-9c66-9e50ad9c4d69',
+            //'processing_status' => '2817418e-5afc-4a8e-b2e4-7e4ff104e095',
+            //'processing_status' => 'a3e2ad64-16e2-4fe3-9c66-9e50ad9c4d69',
             
-            'object_name' => '123'
+            //'object_name' => '123'
+
 			//'DynamicField_nocproserviceid' => array(
 			//	'Equals' => [$id_relacinamento]
             //),
