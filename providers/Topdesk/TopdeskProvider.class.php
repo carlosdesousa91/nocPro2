ef0e169fd13d950fb3d5f6bb288aff9059d113fd
carlosdesousa91 <carlos.desousa91@outlook.com>
2020-10-27 18:06:38 -0300

    refatora classe TopdeskProvider

--- providers/Topdesk/TopdeskProvider.class.php
+++ providers/Topdesk/TopdeskProvider.class.php
@@ -1095,7 +1095,7 @@ class TopdeskProvider extends AbstractProvider {
 			
 			$argument = array(
                     //'action'            => $titulo,
-                    'request'           => $ticket_arguments['Body'] . $ticket_dynamic_fields[1]['Value'],
+                    'request'           => $ticket_arguments['Body'] .  date('yyyy-MM-dd\'T\'HH:mm:ss.SSS', strtotime($ticket_dynamic_fields[1]['Value'])),
                     //'request'           => 'corpo do chamdo',
                     'briefDescription'  => $titulo,
                     //'caller'            =>  array('id' =>  'c3870881-03fa-41b5-a88d-2d65aed12ea8')
