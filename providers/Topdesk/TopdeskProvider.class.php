7f63f8b14578c8b2ca5879426f84d55dab010476
carlosdesousa91 <carlos.desousa91@outlook.com>
2020-10-29 20:01:32 -0300

    deploy producao

--- providers/Topdesk/TopdeskProvider.class.php
+++ providers/Topdesk/TopdeskProvider.class.php
@@ -862,13 +862,13 @@ class TopdeskProvider extends AbstractProvider {
 			
 			$argument = array(
                     //'action'            => $titulo,
-                    'request'           => $ticket_arguments['Body'], /** .  date('Y-m-d\TH:i:s.u', strtotime($ticket_dynamic_fields[1]['Value'])),*/
+                    'request'           => $ticket_arguments['Body'] . $email_cliente, /** .  date('Y-m-d\TH:i:s.u', strtotime($ticket_dynamic_fields[1]['Value'])),*/
                     //'request'           => 'corpo do chamdo',
                     'briefDescription'  => $titulo,
                     //'caller'            =>  array('id' =>  'c3870881-03fa-41b5-a88d-2d65aed12ea8'),
-                    'callerLookup'      => array(
-                        'email' => $email_cliente
-                        ),
+                    //'callerLookup'      => array(
+                    //    'email' => $email_cliente
+                    //    ),
                     //o campo type refere-se ao tipo de chamado, incidente, requisição, etc. No contexto do nocpro ele será usada para outro fim e todos os chamado serão do tipo Incidente
                     'callType'  => array(
                         'name' => 'Incidente'
