16f437892708e8024096bc7c4af11a950b1efa12
Carlos Araujo de Sousa <carlos.sousa@terceiro.rnp.br>
2021-08-11 18:25:54 -0300

    RNP2021-051834Problema com Nocpro.

--- providers/Topdesk/nocPro/topdesk_class.php
+++ providers/Topdesk/nocPro/topdesk_class.php
@@ -955,7 +955,14 @@ function ticketCliente_td($ic_parents_td,$rule_data=array()){
 
 	}
 
-	$parent_campos = consultaIcTopdesk($parent_name, 0, 0, $rule_data=array());
+	$parent_campos = consultaIcTopdesk($parent_name, 0, 0,
+	array(
+		'address' => $this->rule_data['address'],
+		'path' =>  $this->rule_data['path'],
+		'username' =>  $this->rule_data['username'], 
+		'password' =>  $this->rule_data['password']
+	)
+	);
 
 	//$parent_email = $parent_campos[0]['email'];
 
